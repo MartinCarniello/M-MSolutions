98d39949a7502769e684e825ed0eece04dbebdac
Martin Carniello <Martin Carniello@gmail.com>
2015-01-07 20:16:08 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArComMmsolutions::Application.config.secret_key_base = 'c9ec4d31ac70276622dfb0867c28ab68891ed0e5b1e09478078cd4c19967710125fd78a58a28e1831bb43e7a97878b37817aec548e8a6ca1e3e67425b2976fb7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ArComMmsolutions::Application.config.session_store :cookie_store, key: '_ar_com_mmsolutions_session'
